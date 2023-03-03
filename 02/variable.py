dd8334eedde8bc345aaf62e560d53b3d96f46337
Idham Adi Leksono <idhamadileksono@gmail.com>
2023-03-03 10:40:39 +0700

    -m

---
+++ 02/variable.py
@@ -0,0 +1,9 @@
+""" 
+#! 
+"""
+# first_name = "Muhammad"
+# last_name = "Sumbul"
+# full_name = first_name + " "+ last_name
+
+# print("halo bank" + full_name)
+# print(type(full_name))
---
+++ 03/tipe_data.py
@@ -0,0 +1,6 @@
+# a = 10, a adalah variabel dengan nilai 10
+
+#  tipe data: angka satuan (int)
+data_intger = 1
+print("data :" , data_intger)
+print("bertipe : ", type(data_intger))
