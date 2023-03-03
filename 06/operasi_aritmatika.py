9af3c5674ff8e2f4b2ac73f662ef905a6aaa0c9f
Idham Adi Leksono <idhamadileksono@gmail.com>
2023-03-03 19:06:27 +0700

    u

---
+++ 06/operasi_aritmatika.py
@@ -0,0 +1,41 @@
+# operasi aritmatika
+
+a = 10
+b = 3
+
+# operasi tambah
+hasil = a + b
+print(a, "+", b, "=", hasil)
+
+# operasi pengurangan
+hasil = a - b
+print(a, "-", b, "=", hasil)
+
+# operasi perkalian
+hasil = a * b
+print(a, "*", b, "=", hasil)
+
+# operasi pembagian
+hasil = a / b
+print(a, "/", b, "=", hasil)
+
+# operasi eksponen (pangkat)
+hasil = a ** b
+print(a, "**", b, "=", hasil)
+
+# operasi modulus
+hasil = a % b
+print(a, "%", b, "=", hasil)
+
+# operasi floor division (pembulatan)
+hasil = a // b
+print(a, "//", b, "=", hasil)
+
+# prioritas operasi
+a, b, c = 3, 2, 1
+
+hasil = a + b * c
+print(hasil)
+
+hasil =(a + b) * c
+print(hasil)
