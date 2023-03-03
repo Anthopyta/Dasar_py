de5ef4e41bf8faf609712321194f6cf5a58a5894
Idham Adi Leksono <idhamadileksono@gmail.com>
2023-03-03 11:20:44 +0700

    u

---
+++ 04/casting_tipe_data.py
@@ -0,0 +1,11 @@
+# merbah dari satu tipe ke tipe lain 
+
+data_init = 9;
+print("data : ", data_init, ",type =", type(data_init))
+
+data_float = float(data_init)
+data_str = str(data_init)
+data_bool = bool(data_init) # akan false jika nilai int = 0
+print("data : ", data_init, ",type =", type(data_init))
+print("data : ", data_str, ",type =", type(data_init))
+print("data : ", data_bool, ",type =", type(data_init))
