76a073340f3c9450ca097dc2137e0a7e24cdc6d0
Idham Adi Leksono <idhamadileksono@gmail.com>
2023-03-03 10:44:36 +0700

    03 u

--- 03/tipe_data.py
+++ 03/tipe_data.py
@@ -1,6 +1,24 @@
 # a = 10, a adalah variabel dengan nilai 10
 
 #  tipe data: angka satuan (int)
-data_intger = 1
-print("data :" , data_intger)
-print("bertipe : ", type(data_intger))
+data_integer = 1
+print("data :" , data_integer)
+print("bertipe : ", type(data_integer))
+
+# tipe data: kumpulan karakter (string)
+data_string = "ucup"
+print("data :" , data_integer)
+print("bertipe : ", type(data_integer))
+
+# tipe data: biner true/ false (boolean)
+data_boolean = False
+print("data :" , data_integer)
+print("bertipe : ", type(data_integer))
+
+
+# tipe data khusus 
+
+# bilangan kompleks
+data_complex = complex(5, 6)
+print("data :" , data_complex)
+print("bertipe : ", type(data_complex))
