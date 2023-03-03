15f00acbc94de58a38851c5154a4b91b42cb7e1b
Idham Adi Leksono <idhamadileksono@gmail.com>
2023-03-03 14:24:31 +0700

    u

--- 04/casting_tipe_data.py
+++ 04/casting_tipe_data.py
@@ -1,11 +1,46 @@
 # merbah dari satu tipe ke tipe lain 
 
-data_init = 9;
-print("data : ", data_init, ",type =", type(data_init))
+# integer
+print("===INTEGER===")
+data_int = 9
 
-data_float = float(data_init)
-data_str = str(data_init)
-data_bool = bool(data_init) # akan false jika nilai int = 0
-print("data : ", data_init, ",type =", type(data_init))
-print("data : ", data_str, ",type =", type(data_init))
-print("data : ", data_bool, ",type =", type(data_init))
+data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int) # akan false jika nilai int = 0
+print("data : ", data_int, ",type =", type(data_int))
+print("data : ", data_str, ",type =", type(data_int))
+print("data : ", data_bool, ",type =", type(data_int))
+
+# float
+print("===FLOAT===")
+data_float = 1.0
+
+data_int = int(data_float)
+data_str = str(data_float)
+data_bool = bool(data_float) 
+print("data : ", data_int, ",type =", type(data_float))
+print("data : ", data_str, ",type =", type(data_float))
+print("data : ", data_bool, ",type =", type(data_float))
+
+# boolean
+print("===BOOLEAN==")
+data_bool = True
+
+data_int = int(data_bool)
+data_str = str(data_bool)
+data_float = float(data_bool) 
+print("data : ", data_int, ",type =", type(data_bool))
+print("data : ", data_str, ",type =", type(data_bool))
+print("data : ", data_float, ",type =", type(data_bool))
+
+
+# string
+print("===STRING===")
+data_str = '10'
+
+data_int = int(data_str)
+data_float = str(data_str)
+data_bool = bool(data_str) 
+print("data : ", data_int, ",type =", type(data_str))
+print("data : ", data_float, ",type =", type(data_str))
+print("data : ", data_bool, ",type =", type(data_str))
