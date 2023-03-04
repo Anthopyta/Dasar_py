fbb5023544679356048db4423bfc5a10da8bb31c
Idham Adi Leksono <idhamadileksono@gmail.com>
2023-03-04 15:04:44 +0700

    u

---
+++ 09/operasi_bitwise.py
@@ -0,0 +1,14 @@
+# operator bitwise 
+
+a = 9
+b = 5
+
+# bitwise OR
+c = a | b
+print('\n===== OR =====')
+print('nilai :', a, ' binary :', format(a, '08b'))
+print('nilai :', b, ' binary :', format(b, '08b'))
+print('-----------------------------------------(|)')
+print('nilai :', c, ' binary :', format(c, '08b'))
+
+# bitwise AND
