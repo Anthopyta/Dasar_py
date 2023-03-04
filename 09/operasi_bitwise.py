dafb95fa5f28320c3dc9e584c4d183a5cd15f43b
Idham Adi Leksono <idhamadileksono@gmail.com>
2023-03-04 15:06:27 +0700

    m

--- 09/operasi_bitwise.py
+++ 09/operasi_bitwise.py
@@ -1,14 +1,34 @@
 # operator bitwise 
 
-a = 9
-b = 5
+# Operator Bitwise AND
+a = 10  # 1010 dalam biner
+b = 3   # 0011 dalam biner
+c = a & b # 0010 dalam biner
+print(c) # Output: 2
 
-# bitwise OR
-c = a | b
-print('\n===== OR =====')
-print('nilai :', a, ' binary :', format(a, '08b'))
-print('nilai :', b, ' binary :', format(b, '08b'))
-print('-----------------------------------------(|)')
-print('nilai :', c, ' binary :', format(c, '08b'))
+# Operator Bitwise OR
+a = 10  # 1010 dalam biner
+b = 3   # 0011 dalam biner
+c = a | b # 1011 dalam biner
+print(c) # Output: 11
 
-# bitwise AND
+# Operator Bitwise XOR
+a = 10  # 1010 dalam biner
+b = 3   # 0011 dalam biner
+c = a ^ b # 1001 dalam biner
+print(c) # Output: 9
+
+# Operator Bitwise NOT
+a = 10  # 1010 dalam biner
+c = ~a  # -11 dalam desimal (menggunakan representasi 2's complement)
+print(c) # Output: -11
+
+# Operator Bitwise Shift Left
+a = 10  # 1010 dalam biner
+c = a << 2 # 101000 dalam biner
+print(c) # Output: 40
+
+# Operator Bitwise Shift Right
+a = 10  # 1010 dalam biner
+c = a >> 2 # 0010 dalam biner
+print(c) # Output: 2
