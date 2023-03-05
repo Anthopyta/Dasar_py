936c7e388728223b55623eb07971185af62d97d2
Idham Adi Leksono <idhamadileksono@gmail.com>
2023-03-05 16:51:12 +0700

    m

--- 05/input_data.py
+++ 05/input_data.py
@@ -1,15 +1,24 @@
 # input user
 # data yang dimasukan pasti string 
-data = input("Masukan data: ")
+# data = input("Masukan data: ")
 
-print("data : ", data, ", type : ", type(data))
+# print("data : ", data, ", type : ", type(data))
 
-# jika kita ingin mengambil int, maka
-angka = float(input("masukan angka : "))
-angka = int(input("masukan angka : "))
+# jika kita ingin mengambil angka, maka
+# angka = float(input("masukan angka : "))
+# angka = int(input("masukan angka : "))
 
-print("data : ", angka, ", type : ", type(angka))
+# print("data : ", angka, ", type : ", type(angka))
 
 # jika boolean
-biner = bool(int(input("masukan nilai boolean : ")))
-print("data: ", biner, ", type : ", type(biner))
+# biner = bool(int(input("masukan nilai boolean : ")))
+# print("data: ", biner, ", type : ", type(biner))
+
+# contoh
+nama = input("Siapa namamu?: ")
+umur = int(input("Umurmu berapa?: "))
+tinggi = float(input("Tinggi Kamu berapa?: "))
+
+print("Nama saya: " + nama)
+print("Umur saya: " + str(umur)+ " tahun")
+print("Tinggi saya: " +str(tinggi)+ "cm")
