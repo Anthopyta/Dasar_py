0765994ad0ca23430c3f0395c759121582111b6f
Idham Adi Leksono <idhamadileksono@gmail.com>
2023-03-06 14:03:08 +0700

    m

---
+++ 16/while_loop.py
@@ -0,0 +1,14 @@
+# while loop
+
+# while kondisi:
+#    statement
+#    statement
+
+angka = 1
+print(f"nilai awal -> {angka}")
+
+while angka <= 5:
+    angka += 1
+    print(f"nilai awal -> {angka}")
+    
+print("Done")
