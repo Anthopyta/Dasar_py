1d6e365cc4ad2b1b94695d0f077864f331e8ec1d
Idham Adi Leksono <idhamadileksono@gmail.com>
2023-03-19 21:27:49 +0700

    m

--- 19/latihan.py
+++ 19/latihan.py
@@ -8,5 +8,14 @@ print(f"{'PROGRAM MENGHITUNG LUAS':^40}")
 print(f"{'DAN KELILING PERSEGI PANJANG':^40}")
 print(f"{'-'*40:^40}")
 
-# mang
+# input user
+lebar = int(input("Masukan nilai lebar: "))
+panjang = int(input("Masukan nilai panjang: "))
 
+# program menghitung luas
+luas = panjang * lebar
+keliling = 2*(panjang + lebar)
+
+# menampilkan hasil
+print(f"hasil perhitungan luas {luas}")
+print(f"hasil perhitungan keliling {keliling}")
