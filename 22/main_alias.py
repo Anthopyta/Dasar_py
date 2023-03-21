1c993d02d9f64e14a08d304c24b2abda1fe77c07
Idham Adi Leksono <idhamadileksono@gmail.com>
2023-03-22 01:01:23 +0700

    u

---
+++ 22/main_alias.py
@@ -0,0 +1,14 @@
+from matematika import tambah as plus
+from matematika import kali as k
+from matematika import pangkat as apa_aja
+# from matematika import *
+
+
+hasil_tambah = plus(1,2,3,4,5)
+print(f"hasil tambah: {hasil_tambah}")
+
+hasil_kali = k(1,2,3,4,5)
+print(f"hasil tambah: {hasil_kali}")
+
+pangkat_3 = apa_aja(3)
+print(f"hasil pangkat 3: {pangkat_3(3)}")
