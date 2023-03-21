3a290670eca0aa4a858868fd0a554440253dbd8d
Idham Adi Leksono <idhamadileksono@gmail.com>
2023-03-22 06:47:48 +0700

    m

--- 24/main_app.py
+++ 24/main_app.py
@@ -2,3 +2,9 @@ import sains
 
 hasil_tambah = sains.matematika.tambah(1, 2, 3, 4, 5)
 print(f"hasi tambah: {hasil_tambah}")
+
+hasil_fisika = sains.fisika.gaya(10, 9.8)
+print(f"hasi tambah: {hasil_fisika}")
+
+hasil_kali = sains.kali(1, 2, 3, 4, 5)
+print(f"hasi tambah: {hasil_kali}")
